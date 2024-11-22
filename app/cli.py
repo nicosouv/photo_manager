744aaf7434be69129d342474d15c8a209ac43342
Nico Souveton <nicosouv@protonmail.com>
2024-11-22 14:28:11 +0100

    use ruff and tooling, setup pyproject.toml

--- app/cli.py
+++ app/cli.py
@@ -1,49 +1,14 @@
 import argparse
 from config.settings import Config
-from data.drive_client import list_files_in_drive, download_file
-from data.file_parser import is_google_drive_path, parse_local_files
-from config.secrets_manager import get_credentials
-from googleapiclient.discovery import build
-import os
-from pathlib import Path
 from config.logging_config import Logger
+from data.file_parser import process_input_path
 
 logger = Logger().logger
 
 
 def process_photos(input_path: str) -> None:
     """Process photos from a given directory path, supporting both Google Drive and local paths."""
-
-    TEMP_FOLDER = "/tmp"  # This should be defined or configured properly
-
-    if is_google_drive_path(input_path):
-        logger.info("Detected Google Drive path.")
-
-        try:
-            credentials = get_credentials()
-            service = build("drive", "v3", credentials=credentials)
-            drive_files = list_files_in_drive(service, input_path)
-
-            if not os.path.exists(TEMP_FOLDER):
-                os.makedirs(TEMP_FOLDER)
-
-            for drive_file in drive_files:
-                destination = os.path.join(TEMP_FOLDER, drive_file["name"])
-                download_file(service, drive_file["id"], destination)
-                logger.info(f"Downloaded {drive_file['name']} to {TEMP_FOLDER}")
-
-        except Exception as e:
-            logger.error(f"An error occurred while processing Google Drive files: {e}")
-
-    else:
-        logger.info(f"Detected local path")
-        try:
-            local_files = parse_local_files(Path(input_path), extensions=[".heic", ".jpg", ".png"])
-            for file in local_files:
-                logger.info(f"Processing local file: {file}")
-
-        except Exception as e:
-            logger.error(f"An error occurred while processing local files: {e}")
+    process_input_path(input_path)
 
 
 def main():
--- app/data/file_parser.py
+++ app/data/file_parser.py
@@ -1,32 +1,63 @@
-from typing import List, Dict, Union
+from typing import List, Union
+from data.drive_client import list_files_in_drive, download_file
+from config.secrets_manager import get_credentials
+from googleapiclient.discovery import build
+import os
 from pathlib import Path
-from googleapiclient.discovery import Resource
+from config.logging_config import Logger
+
+
+logger = Logger().logger
 
 
 def is_google_drive_path(path: str) -> bool:
-    """Détecte si un chemin correspond à un ID Google Drive."""
+    """Detect if path is similar to a gDrive ID"""
     return len(path) == 33 and not Path(path).exists()
 
 
-def parse_local_files(directory: Path, extensions: List[str]) -> List[Path]:
-    """Parse les fichiers dans un répertoire local."""
+def list_files(directory: Path, extensions: List[str]) -> List[Path]:
+    """Parse and list local files"""
     return [f for f in directory.iterdir() if f.suffix.lower() in extensions]
 
 
-def list_files_in_drive(service: Resource, folder_id: str) -> List[Dict[str, str]]:
-    """Liste les fichiers dans un dossier Google Drive donné."""
-    query = f"'{folder_id}' in parents and trashed=false"
-    results = service.files().list(q=query).execute()
-    return results.get("files", [])
+def parse_local_files(input_path: str):
+    try:
+        local_files = list_files(Path(input_path), extensions=[".heic", ".jpg", ".png"])
+        logger.info(f"Found {len(local_files)} local files.")
+        for file in local_files:
+            logger.info(f"Processing local file: {file}")
+
+        return local_files
+
+    except Exception as e:
+        logger.error(f"An error occurred while processing local files: {e}")
 
 
-def process_input_path(service: Resource, input_path: Union[str, Path]) -> None:
-    """Traite les fichiers à partir d'un chemin local ou Google Drive."""
-    if isinstance(input_path, str) and is_google_drive_path(input_path):
-        print("Processing Google Drive folder")
-        drive_files = list_files_in_drive(service, input_path)
-        print(f"Found {len(drive_files)} files in Google Drive.")
-    elif isinstance(input_path, Path):
-        print("Processing local folder")
-        local_files = parse_local_files(input_path, [".heic", ".png", ".jpg"])
-        print(f"Found {len(local_files)} local files.")
+def process_input_path(input_path: Union[str, Path]) -> None:
+    temp_folder: str = "./tmp"  # This should be defined or configured properly
+
+    if is_google_drive_path(input_path):
+        logger.info("Detected Google Drive path.")
+
+        try:
+            credentials = get_credentials()
+            service = build("drive", "v3", credentials=credentials)
+            drive_files = list_files_in_drive(service, input_path)
+            logger.info(f"Found {len(drive_files)} files in Google Drive.")
+
+            if not os.path.exists(temp_folder):
+                os.makedirs(temp_folder)
+
+            for drive_file in drive_files:
+                destination = os.path.join(temp_folder, drive_file["name"])
+                download_file(service, drive_file["id"], destination)
+                logger.info(f"Downloaded {drive_file['name']} to {temp_folder}")
+
+            parse_local_files(temp_folder)
+
+        except Exception as e:
+            logger.error(f"An error occurred while processing Google Drive files: {e}")
+
+    else:
+        logger.info("Detected local path")
+        parse_local_files(input_path)
